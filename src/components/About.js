2b5080ac93d4b09229237f82483fff922416b0c4
+ <+>
2023-07-20 22:29:39 +0300

    completed assignment

--- src/components/About.js
+++ src/components/About.js
@@ -4,11 +4,12 @@ import Links from "./Links";
 
 //function About() {
   function About(props){
+    const bio = props.bio
   return (
     <div id="about">
       <h2>About Me</h2>
-      <p>Put the bio in here</p>
       <p> {props.bio}</p>
+      {bio ? <p>{props.bio}</p> : ""}
       <img src="https://i.imgur.com/mV8PQxj.gif" alt="I made this" />
       {/* add your <Links /> component here */}
       <Links github = {props.github} linkedin = {props.linkedin}/>
--- src/components/App.js
+++ src/components/App.js
@@ -12,8 +12,8 @@ function App() {
   return (
     <div>
       <NavBar />
-      <Home />
-      <About />
+      {/* <Home />
+      <About /> */}
       <Home color={user.color} name={user.name} city={user.city} />
       <About bio={user.bio} github={user.links.github} linkedin={user.links.linkedin} />
     </div>
